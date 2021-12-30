6b3af2811bc3d6261e641ca53e9e9035f0fe7264
emmanue <brinkcorp@gmail.com>
2021-12-30 18:13:20 +0000

    Increment

--- app/src/main/java/com/example/android/gdgfinder/BindingAdapters.kt
+++ app/src/main/java/com/example/android/gdgfinder/BindingAdapters.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Following best practices
+// Following best practices with solid principles
 
 package com.example.android.gdgfinder
 

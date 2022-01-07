23c7109feb63e05c5b2e9b01be9863c1a4af0804
emmanue <brinkcorp@gmail.com>
2022-01-07 18:19:06 +0000

    Increment

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ //Separation of concern
 // Following best practices with solid principles
 
 package com.example.android.gdgfinder.home

c8ccb6a17e1bd4553471a238c2f0cc478b42728b
emmanue <brinkcorp@gmail.com>
2022-01-08 16:34:18 +0000

    Increment

--- app/src/main/java/com/example/android/gdgfinder/network/GdgChapter.kt
+++ app/src/main/java/com/example/android/gdgfinder/network/GdgChapter.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+ //Separation of concern
  // Following best practices with solid principles
 
 
